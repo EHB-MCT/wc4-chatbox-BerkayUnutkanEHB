b113c60dd8d5b75669908a1b52c03a13d6d885c0
Berkay Unutkan <berkay.unutkan@ehb.be>
2023-03-15 16:32:40 +0100

    chatbox

--- js/index.js
+++ js/index.js
@@ -5,12 +5,34 @@
 const chat = {
     author: "yourName",
     init() {
+        this.fetchMessages();
     },
     sendMessage() {
     },
     fetchMessages() {
+        fetch('https://dev2chat.onrender.com/messages')
+        .then(function(response){
+            return response.json();
+        })
+        .then(function(all){
+            console.log(all);
+            let message = all [0];
+            console.log(message);
+            chat.renderMessage(message);
+        })
     },
     renderMessage(message) {
+        const htmlString = `
+        <div class="messageItem">
+          <div class="header">
+              <span class="author">${message.author}</span>
+              <span class="time">${message.created_at}</span>
+          </div>
+          <p>
+              ${message.message}
+          </p>
+      </div>
+      </div>`
     }
 
 }
